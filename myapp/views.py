aacf0b4d7e89c8b05c9e4cf52d7005e478dd82a6
Ranjan Verma <ranjanverma@Ranjans-MacBook-Air.local>
2023-11-26 15:07:22 +0530

    Saving Links to Databases

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from .models import Link
 # Create your views here.
 
 
@@ -8,9 +9,12 @@ def scrape(request):
     page = requests.get("http://www.google.com")
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    link_address = []
 
     for link in soup.find_all('a'):
-        link_address.append(link.get('href'))
+        link_address = link.get('href')
+        link_text = link.string
+        Link.objects.create(address=link_address, name = link_text)
     
-    return render(request, 'myapp/result.html', {'link_address': link_address})
+    data = Link.objects.all()
+    
+    return render(request, 'myapp/result.html',{'data': data})
